07099e2ce9f6dce3f84b2b5ee4c65ae8c4d5bfe3
Ismail Abdul Kareem <101706647+ismailjutt010@users.noreply.github.com>
2023-11-01 18:40:59 +0500

    firebase testing , when application is running in backgroud and when application is open we are sending notifications ,

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:demonitification/home_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
- await Firebase.initializeApp();
-  runApp( MyApp());
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(
+      _firebaseMessagingBackgroudHandler );
+  runApp(const MyApp());
+}
+ @pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroudHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print(message.notification!.title.toString());
 }
 
 class MyApp extends StatelessWidget {
